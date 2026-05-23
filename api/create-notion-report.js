464108e5b5537c6bcd5fef751ba7f66b22a1d45a
HermanTaniwan <taniwanherman@yahoo.com>
2026-05-23 11:40:19 +0700

    Update create-notion-report.js
    tambah titik

--- api/create-notion-report.js
+++ api/create-notion-report.js
@@ -2,6 +2,6 @@ export default function handler(req, res) {
   return res.status(200).json({
     ok: true,
     method: req.method,
-    message: "create-notion-report endpoint is alive",
+    message: "create-notion-report endpoint is alive.",
   });
 }
